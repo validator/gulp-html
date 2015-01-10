1e9db95d90c9bba8257462998a8e92c91a489525
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-01-10 18:54:40 +0900

    Update test code

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
 var gulp   = require('gulp');
 var validator = require('./');
 
-gulp.task('ok', function() {
+gulp.task('ok', function () {
+  var status = true;
   return gulp.src('./test/ok.html')
-  .pipe(validator());
+  .pipe(validator())
+  .on('error', function () {
+    status = false;
+  })
+  .on('end', function () {
+    if (status === true) return;
+    throw new Error("ok.html isn't ok");
+  });
 });
 
-gulp.task('ng', function() {
+gulp.task('ng', function () {
+  var status = true;
   return gulp.src('./test/ng.html')
-  .pipe(validator());
+  .pipe(validator())
+  .on('error', function () {
+    status = false;
+  })
+  .on('end', function () {
+    if (status === false) return;
+    throw new Error("ng.html isn't ng");
+  });
 });
+
+gulp.task('test', ['ok']);
