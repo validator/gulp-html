b8d77532687762e70268decdbc01bf1da2feb44d
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-01-11 01:16:52 +0900

    Add options

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
+var exec = require('child_process').exec;
 var through = require('through2');
 var gutil = require('gulp-util');
+var merge = require('merge');
 var PluginError = gutil.PluginError;
-var exec = require('child_process').exec;
-var vnu = 'java -jar vnu.jar -';
+var vnu = 'java -jar vnu.jar ';
 
-module.exports = function() {
+module.exports = function(opt) {
   var stream  = through.obj(function(file, enc, cb) {
-    this.push(file);
-    if (file.isNull()) {
-      // Do nothing if no contents
-    }
-
-    if (file.isBuffer()) {
-      var str = file.contents.toString('utf8');
-      exec("echo '" + str + "' | " + vnu, function (err, stdout, stderr) {
-        if (err === null) {
-          return cb();
-        }
-        cb(new PluginError('gulp-html', stderr));
-        return;
-      });
-    }
-
+    if (file.isNull()) return cb(null, file);
     if (file.isStream()) {
-      cb(new PluginError('gulp-html', 'Only Buffers are supported'));
-      return;
+      return cb(new PluginError('gulp-html', 'Streaming not supported'));
     }
+    var str = file.contents.toString('utf8');
+    var options = merge({
+      'errors-only': false,
+      'format': 'gnu',
+      'html': false,
+      'no-stream': false,
+      'verbose': false,
+    }, opt);
+
+    // Set options
+    Object.keys(options).forEach(function (key) {
+      var val = options[key];
+      if (key === 'format' && val !== 'gnu') vnu += '--format ' + val + ' ';
+      if (val === true) vnu += '--' + key + ' ';
+    });
+
+    exec("echo '" + str + "' | " + vnu + '-', function (err, stdout, stderr) {
+      if (err === null) return cb(null, file);
+      return cb(new PluginError('gulp-html', stderr));
+    });
   });
 
   return stream;
