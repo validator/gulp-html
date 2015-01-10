56096c1c71ea199571a8799726c7f239314a874b
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-01-10 18:54:52 +0900

    Add example and update index

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gulp-html
 gulp plugin for html validation
+
+
+## Usage
+
+```js
+var gulp   = require('gulp');
+var validator = require('gulp-html');
+
+gulp.task('html', function() {
+  return gulp.src('src/index.html')
+  .pipe(validator())
+  .pipe(gulp.dest('dist/'));
+});
+```
--- index.js
+++ index.js
@@ -2,20 +2,23 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var exec = require('child_process').exec;
-var command = 'java -jar vnu.jar -';
-var err;
+var vnu = 'java -jar vnu.jar -';
 
 module.exports = function() {
   var stream  = through.obj(function(file, enc, cb) {
+    this.push(file);
     if (file.isNull()) {
       // Do nothing if no contents
     }
 
     if (file.isBuffer()) {
       var str = file.contents.toString('utf8');
-      exec("echo '" + str + "' | " + command, function (err, stdout, stderr) {
-        if (err === null) return;
-        throw new PluginError('gulp-html', stderr);
+      exec("echo '" + str + "' | " + vnu, function (err, stdout, stderr) {
+        if (err === null) {
+          return cb();
+        }
+        cb(new PluginError('gulp-html', stderr));
+        return;
       });
     }
 
@@ -23,8 +26,6 @@ module.exports = function() {
       cb(new PluginError('gulp-html', 'Only Buffers are supported'));
       return;
     }
-
-    return cb();
   });
 
   return stream;
